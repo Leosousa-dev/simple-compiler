2566cf172ba7922db3a818a8f4c24ff73b209b7d
leonardo <leonardocasa790@gmail.com>
2023-12-04 15:16:34 -0300

    chore: import sections compiler

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import compiler from "./compiler/compiler";
 
 const input: string = "(sum(4,5))"
 
-const out = compiler
+const out = compiler(input);
+
+console.log(out)
+console.log(JSON.stringify(out, null, 2));
