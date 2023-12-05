0a212f726023fe1003cbce7aa7db677d23bfdbbb
leonardo <leonardocasa790@gmail.com>
2023-12-05 12:55:36 -0300

    chore: create fn recognize parents

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,11 @@ export function isChar(char: string): boolean{
    return (char >= 'a' &&  char <= 'z' || char >= 'A' && char <= 'Z')
 }
 
+export function isParent(char: string): boolean{
+   return char === "(" || char === ")"
+}
+
+
 export function isDigit(char: string): boolean{
 
    return char >= '0' && char <= '9'
